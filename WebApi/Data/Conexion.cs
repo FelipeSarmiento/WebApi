9ab7c5bf5b58fda33f327955a8d5bcfd636e49b0
Felipe Sarmiento <felipesarmiento@gmail.com>
2023-01-09 12:41:28 -0500

    Segundo Commit Aplicacion Web

--- WebApi/Data/Conexion.cs
+++ WebApi/Data/Conexion.cs
@@ -7,6 +7,6 @@ namespace WebApi.Data
 {
     public class Conexion
     {
-        public static string rutaConexion = "Data Source=FELIPEPC\\FELIPE;Initial Catalog=DBPRUEBAS;Integrated Security=True";
+        public static string rutaConexion = @"Server=tcp:server-felipe.database.windows.net,1433;Initial Catalog=sqlprueba;Persist Security Info=False;User ID=felipe;Password=Pipe0803;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
     }
 }
